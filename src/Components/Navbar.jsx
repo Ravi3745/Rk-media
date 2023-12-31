5933bb45f7784fbfbc5a89dd10580f00d6cac97c
Kant Sharma <ravi.kant-sharma@capgemini.com>
2023-12-31 17:16:01 +0530

    YouTube clone completed

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Stack, Typography } from '@mui/material';
+import {Link} from 'react-router-dom';
+import { logo } from '../utils/constants';
+import SearchBar from './SearchBar';
+const Navbar = () => {
+
+ 
+  return (
+    <Stack direction='row' alignItem='center' p={2} sx={{position:'sticky', background:'#000', top:0, justifyContent:'space-between'}}>
+
+      <Link to='/' style={{display:'flex', alignItems:'center'}}>
+          <img src={logo} alt='logo' height={25}/>
+
+          <Typography variant='h6' color='white' fontWeight='700' ml={2} sx={{ display: { xs: 'none', md: 'block' } }} >
+            YouTube
+          </Typography>
+          
+      </Link>
+        {/* search bar component  */}
+      <SearchBar />
+    
+    </Stack>
+  )
+}
+ export default Navbar;
